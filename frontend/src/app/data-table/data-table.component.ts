043a35109420ac9abe7d60a6a7054080e735862c
leonardoturriagovargas <leonardoturriagovargas@gmail.com>
2025-01-11 03:25:14 +0000

    Fetch and show data

--- frontend/src/app/data-table/data-table.component.ts
+++ frontend/src/app/data-table/data-table.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient , HttpClientModule} from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-data-table',
-  imports: [],
+  imports: [CommonModule,HttpClientModule],
   templateUrl: './data-table.component.html',
   styleUrl: './data-table.component.css'
 })
-export class DataTableComponent {
+export class DataTableComponent implements OnInit{
+  httpClient = inject(HttpClient);
+  data: any[] = [];
 
+  ngOnInit(): void {this.fetchData()}
+  // ngOnInit(): void {console.log("hola");}
+
+  fetchData(){
+    this.httpClient
+    .get<any[]>('https://localhost:3000')
+    .subscribe((data)=>{
+      console.log(data);
+      this.data=data;
+    });
+  }
 }
